0c1a392269e8ed95d049557ccae94119f74b26f0
Arturo Gutierrez <agutierrez.gyo@gmail.com>
2026-04-24 15:08:57 -0300

    Initial Commit

---
+++ HolaSwift/HolaSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HolaSwiftApp.swift
+//  HolaSwift
+//
+//  Created by Arturo Gutierrez on 24/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct HolaSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
